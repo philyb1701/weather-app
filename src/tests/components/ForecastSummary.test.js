e79d7704c38d522807fe2934907ab23d8e68add9
Phil Burkhill <philip.burkhill@gmail.com>
2022-06-06 21:41:41 +0100

    ForecastSummary testing

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -2,4 +2,27 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ForecastSummary from "../../components/ForecastSummary";
 
-describe("ForecastSummary", () => {});
+describe("ForecastSummary", () => {
+  const validProps = {
+    date: 11111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders data correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
